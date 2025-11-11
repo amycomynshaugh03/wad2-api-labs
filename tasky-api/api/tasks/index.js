c0d34fb23f6de9f1dde01223551c5814f07ffcee
Amy Comyns Haugh <comynsamy03@gmail.com>
2025-11-11 13:48:27 +0000

    Express Lab - exercise done

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -21,33 +21,48 @@ router.get('/:id', (req, res) => {
     return res.status(200).json(task);
 });
 
-//Add a task
+// Add a task
 router.post('/', (req, res) => {
     const { title, description, deadline, priority, done } = req.body;
+    const now = new Date().toISOString();
+
     const newTask = {
         id: uuidv4(),
         title,
         description,
         deadline,
         priority,
-        done
+        done,
+        created_at: now,
+        updated_at: now
     };
+
     tasksData.tasks.push(newTask);
-    res.status(201).json(newTask);
     tasksData.total_results++;
+    res.status(201).json(newTask);
 });
 
-//Update an existing task
+// Update an existing task
 router.put('/:id', (req, res) => {
     const { id } = req.params;
     const taskIndex = tasksData.tasks.findIndex(task => task.id === id); 
+
     if (taskIndex === -1) {
         return res.status(404).json({ status: 404, message: 'Task not found' });
     }
-    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, id:id };
+
+    const now = new Date().toISOString();
+    const updatedTask = { 
+        ...tasksData.tasks[taskIndex], 
+        ...req.body, 
+        id: id,
+        updated_at: now 
+    };
+
     tasksData.tasks[taskIndex] = updatedTask;
     res.json(updatedTask);
 });
+
 
 //Delete a task
 router.delete('/:id', (req, res) => {
@@ -60,6 +75,8 @@ router.delete('/:id', (req, res) => {
     res.status(204).send();
     tasksData.total_results--;
 });
+
+
 
 
 
