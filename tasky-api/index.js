6da67c00a57a72f31e7a83be69a77f9693edb553
Amy Comyns Haugh <comynsamy03@gmail.com>
2025-11-18 14:41:44 +0000

    Mongo lab Error Handling

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -6,6 +6,14 @@ import './db';
 
 dotenv.config();
 
+const errHandler = (err, req, res, next) => {
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
+};
+
+
 const app = express();
 
 const port = process.env.PORT;
@@ -13,6 +21,8 @@ const port = process.env.PORT;
 app.use(express.json());
 
 app.use('/api/tasks', tasksRouter);
+
+app.use(errHandler);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
